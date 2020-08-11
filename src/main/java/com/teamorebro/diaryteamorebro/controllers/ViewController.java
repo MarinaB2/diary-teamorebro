01367550dfce7c123299ef2760deb0efa99d779f
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-08-11 16:10:41 +0200

    Added : index, addEntr and editEntry. Added some method to the view controller

--- src/main/java/com/teamorebro/diaryteamorebro/controllers/ViewController.java
+++ src/main/java/com/teamorebro/diaryteamorebro/controllers/ViewController.java
@@ -1,13 +1,32 @@
 package com.teamorebro.diaryteamorebro.controllers;
 
+import com.teamorebro.diaryteamorebro.DiaryTeamorebroApplication;
+import com.teamorebro.diaryteamorebro.models.Entry;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 public class ViewController {
 
     @GetMapping("/")
-    public String index(){
+    public String listEntries(Model model){
+       // ArrayList<Entry> entries = DiaryTeamorebroApplication.;
+       // model.addAttribute("entries", entries);
         return "/index";
+    }
+
+    @GetMapping("/add")
+    public String addNewEntry() {
+        return "addEntry";
+    }
+    @GetMapping("/edit")
+    public String editEntry(@RequestParam int id, Model model) {
+     //   Entry entry = EntryController.;
+      //  model.addAttribute(entry);
+        return "editEntry";
     }
 }
