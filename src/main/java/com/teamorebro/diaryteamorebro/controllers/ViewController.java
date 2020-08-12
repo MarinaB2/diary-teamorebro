d326bc5c857e2f5d724e98bbe97aa648e56705a0
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-08-12 12:15:17 +0200

    fixed merge error

--- src/main/java/com/teamorebro/diaryteamorebro/controllers/ViewController.java
+++ src/main/java/com/teamorebro/diaryteamorebro/controllers/ViewController.java
@@ -24,7 +24,7 @@ public class ViewController {
         return "/index";
     }
 
-    @PostMapping("/add")
+    @GetMapping("/add")
     public String addNewEntry() {
         return "addEntry";
     }
