220e3b2ca16aecee13ad8c81ae740abd6fc417a5
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-08-12 08:17:40 +0200

    added php file and  tried to fix the style but it doesn't work

--- src/main/java/com/teamorebro/diaryteamorebro/models/Entry.java
+++ src/main/java/com/teamorebro/diaryteamorebro/models/Entry.java
@@ -1,9 +1,5 @@
 package com.teamorebro.diaryteamorebro.models;
 
-
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
 import javax.persistence.*;
 import java.util.Date;
 
@@ -13,6 +9,9 @@ public class Entry {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     public Integer id;
+
+    @Column(nullable = false)
+    public String title;
 
     @Column(nullable = false)
     public String content;
